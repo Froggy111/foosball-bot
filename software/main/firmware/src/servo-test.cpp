8c525c21c10f342adc8d36b3352830ab83b05f20
apandayouknow <yuroutan17@gmail.com>
2025-01-13 01:47:53 +0800

    servo code

--- software/main/firmware/src/servo-test.cpp
+++ software/main/firmware/src/servo-test.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
-#include <RP2040_PWM.h>
 #include "types.hpp"
 #include "stepper.hpp"
-#include "servo.hpp"
 #include "debug.hpp"
 #include <Servo.h>
 
@@ -15,24 +13,21 @@ const u8 servo_dir = 15;
 
 const u8 led_pin = 25;
 
-arm::Servo servo(servo_pin_num, servo_dir);
+Servo servo;
 
 void setup() {
     Serial.begin(115200);
-    pinMode(25, OUTPUT);
-    digitalWrite(25, HIGH);
-    pinMode(15, OUTPUT);
-    digitalWrite(15, HIGH);
-    servo.begin();
-    digitalWrite(25, LOW);
+    pinMode(led_pin, OUTPUT);
+    digitalWrite(led_pin, HIGH);
+    pinMode(servo_dir, OUTPUT);
+    digitalWrite(servo_dir, HIGH);
+    servo.attach(servo_pin_num);
     delay(1000);
 }
 
 void loop() {
-    delay(3000);
-    digitalWrite(25, HIGH);
-    servo.move(1800);
-    delay(3000);
-    digitalWrite(25, LOW);
-    servo.move(0);
+    servo.write(180);
+    delay(1000);
+    servo.write(0);
+    delay(1000);
 }
