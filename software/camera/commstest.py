3e5db3eaa83ca65a756d2c63e923ccad10b7b22f
Froggy111 <lijiayi4789@gmail.com>
2025-01-14 01:56:33 +0800

    comns test

--- software/camera/commstest.py
+++ software/camera/commstest.py
@@ -2,9 +2,27 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM1',115200)
 
+mask_8bit = 0xFF
+mask_16bit = 0xFFFF
+mask_32bit = 0xFFFFFFFF
 
-sent = bytearray([132,3,100,100,100])
-ser.write(sent)
-print(sent)
+type_select = 0b10000000 & mask_8bit # stepper
+payload_size = 13 & mask_8bit # 1 for command option, 4 for pos, 4 for speed, 4 for accel
+first_byte = (type_select | payload_size) & mask_8bit
+command_option = 3 & mask_8bit # move
+pos = 100 & mask_32bit
+speed = 100 & mask_32bit
+accel = 100 & mask_32bit
+
+packet = bytearray([
+    first_byte,
+    command_option,
+    pos & 0xFF, (pos >> 8) & 0xFF, (pos >> 16) & 0xFF, (pos >> 24) & 0xFF,
+    speed & 0xFF, (speed >> 8) & 0xFF, (speed >> 16) & 0xFF, (speed >> 24) & 0xFF, # little endian
+    accel & 0xFF, (accel >> 8) & 0xFF, (accel >> 16) & 0xFF, (accel >> 24) & 0xFF
+])
+
+ser.write(packet)
+print(packet)
 
 
