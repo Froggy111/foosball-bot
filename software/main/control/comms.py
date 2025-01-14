d2089b4ca9258935f130dea5a684c1717241f06f
Froggy111 <lijiayi4789@gmail.com>
2025-01-14 14:40:02 +0800

    idk

--- software/main/control/comms.py
+++ software/main/control/comms.py
@@ -91,7 +91,6 @@ stepper_move_complete_ack_val = 0b01010101
 class Arm:
     def __init__(self, id: int) -> None:
         self._setup_serial(id)
-        self._sent_command = False
     def _setup_serial(self, id: int) -> None:
         vid = "2E8A" # usb vid
         pid = f"FFF{id+1}" # usb pid
@@ -110,47 +109,47 @@ class Arm:
     # actions
     def estop(self) -> None:
         payload = bytearray([
-            TargetCore.core0 & Core0PayloadLengths.estop, # first byte
+            TargetCore.core0 | Core0PayloadLengths.estop, # first byte
             Core0CommandIDs.estop # command id byte
         ])
         self._send_cmd(payload)
     def restart(self) -> None:
         payload = bytearray([
-            TargetCore.core0 & Core0PayloadLengths.restart, # first byte
+            TargetCore.core0 | Core0PayloadLengths.restart, # first byte
             Core0CommandIDs.restart # command id byte
         ])
         self._send_cmd(payload)
     def move_servo(self, angle: int) -> None:
         payload = bytearray([
-            TargetCore.core0 & Core0PayloadLengths.move_servo, # first byte
+            TargetCore.core0 | Core0PayloadLengths.move_servo, # first byte
             Core0CommandIDs.move_servo # command id byte
         ] + to_bytelist_16bit(angle)) # data bytes
         self._send_cmd(payload)
 
     def stepper_enable(self) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.enable, # first byte
+            TargetCore.core1 | Core1PayloadLengths.enable, # first byte
             Core1CommandIDs.enable # command id byte
         ])
         self._send_cmd(payload)
         self._block_until_complete_ack()
     def stepper_disable(self) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.disable, # first byte
+            TargetCore.core1 | Core1PayloadLengths.disable, # first byte
             Core1CommandIDs.disable # command id byte
         ])
         self._send_cmd(payload)
         self._block_until_complete_ack()
     def stepper_reset(self) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.reset, # first byte
+            TargetCore.core1 | Core1PayloadLengths.reset, # first byte
             Core1CommandIDs.reset # command id byte
         ])
         self._send_cmd(payload)
         self._block_until_complete_ack()
     def stepper_move(self, move_pos: int, speed: int, accel: int) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.move, # first byte
+            TargetCore.core1 | Core1PayloadLengths.move, # first byte
             Core1CommandIDs.move # command id byte
         ] + to_bytelist_32bit(move_pos, True) # data bytes
           + to_bytelist_32bit(speed)
@@ -162,7 +161,7 @@ class Arm:
         assert stepper_move_complete_ack == stepper_move_complete_ack_val, "Stepper move complete ack value mismatch."
     def stepper_home(self, moveforward_mm: int, movebackward_mm: int, target_pos: int, speed: int, accel: int) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.home, # first byte
+            TargetCore.core1 | Core1PayloadLengths.home, # first byte
             Core1CommandIDs.home, # command id byte
             moveforward_mm & mask8,
             movebackward_mm & mask8,
@@ -176,63 +175,63 @@ class Arm:
     # config reads
     def stepper_fault_state(self) -> bool:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.fault_state, # first byte
+            TargetCore.core1 | Core1PayloadLengths.fault_state, # first byte
             Core1CommandIDs.fault_state # command id byte
         ])
         self._send_cmd(payload)
         return bool(self.serial_port.read(1))
     def stepper_get_max_speed(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_max_speed, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_max_speed, # first byte
             Core1CommandIDs.get_max_speed # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(4))
     def stepper_get_max_accel(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_max_accel, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_max_accel, # first byte
             Core1CommandIDs.get_max_accel # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(4))
     def stepper_get_steps_per_rev(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_steps_per_rev, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_steps_per_rev, # first byte
             Core1CommandIDs.get_steps_per_rev # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(2))
     def stepper_get_mm_per_rev(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_mm_per_rev, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_mm_per_rev, # first byte
             Core1CommandIDs.get_mm_per_rev # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(1))
     def stepper_get_um_per_step(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_um_per_step, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_um_per_step, # first byte
             Core1CommandIDs.get_um_per_step # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(2))
     def stepper_get_max_steps_per_second(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_max_steps_per_second, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_max_steps_per_second, # first byte
             Core1CommandIDs.get_max_steps_per_second # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(4))
     def stepper_get_max_steps_accel(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_max_steps_accel, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_max_steps_accel, # first byte
             Core1CommandIDs.get_max_steps_accel # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(4))
     def stepper_get_microsteps(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_microsteps, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_microsteps, # first byte
             Core1CommandIDs.get_microsteps # command id byte
         ])
         self._send_cmd(payload)
@@ -241,28 +240,28 @@ class Arm:
     # config writes
     def stepper_set_max_speed(self, max_speed: int) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.set_max_speed, # first byte
+            TargetCore.core1 | Core1PayloadLengths.set_max_speed, # first byte
             Core1CommandIDs.set_max_speed # command id byte
         ] + to_bytelist_32bit(max_speed))
         self._send_cmd(payload)
         self._block_until_complete_ack()
     def stepper_set_max_accel(self, max_accel: int) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.set_max_accel, # first byte
+            TargetCore.core1 | Core1PayloadLengths.set_max_accel, # first byte
             Core1CommandIDs.set_max_accel # command id byte
         ] + to_bytelist_32bit(max_accel))
         self._send_cmd(payload)
         self._block_until_complete_ack()
     def stepper_set_steps_per_rev(self, steps_per_rev: int) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.set_steps_per_rev, # first byte
+            TargetCore.core1 | Core1PayloadLengths.set_steps_per_rev, # first byte
             Core1CommandIDs.set_steps_per_rev # command id byte
         ] + to_bytelist_16bit(steps_per_rev))
         self._send_cmd(payload)
         self._block_until_complete_ack()
     def stepper_set_mm_per_rev(self, mm_per_rev: int) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.set_mm_per_rev, # first byte
+            TargetCore.core1 | Core1PayloadLengths.set_mm_per_rev, # first byte
             Core1CommandIDs.set_mm_per_rev, # command id byte
             mm_per_rev & mask8
         ])
@@ -270,7 +269,7 @@ class Arm:
         self._block_until_complete_ack()
     def stepper_set_microsteps(self, microsteps: int) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.set_microsteps, # first byte
+            TargetCore.core1 | Core1PayloadLengths.set_microsteps, # first byte
             Core1CommandIDs.set_microsteps, # command id byte
             microsteps & mask8
         ])
@@ -280,28 +279,28 @@ class Arm:
     # state machine
     def stepper_get_step_coord(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_step_coord, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_step_coord, # first byte
             Core1CommandIDs.get_step_coord # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(4), True)
     def stepper_get_um_coord(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_um_coord, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_um_coord, # first byte
             Core1CommandIDs.get_um_coord # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(4), True)
     def stepper_get_current_speed(self) -> int:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.get_current_speed, # first byte
+            TargetCore.core1 | Core1PayloadLengths.get_current_speed, # first byte
             Core1CommandIDs.get_current_speed # command id byte
         ])
         self._send_cmd(payload)
         return from_bytelist(self.serial_port.read(4), True)
     def stepper_set_step_coord(self, step_coord: int) -> None:
         payload = bytearray([
-            TargetCore.core1 & Core1PayloadLengths.set_step_coord, # first byte
+            TargetCore.core1 | Core1PayloadLengths.set_step_coord, # first byte
             Core1CommandIDs.set_step_coord, # command id byte
         ] + to_bytelist_32bit(step_coord))
         self._send_cmd(payload)
