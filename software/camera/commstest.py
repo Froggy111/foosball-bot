bf999af240da41ea3b4d4151c4e504275f182f51
Froggy111 <lijiayi4789@gmail.com>
2025-01-14 11:23:19 +0800

    idk

--- software/camera/commstest.py
+++ software/camera/commstest.py
@@ -13,8 +13,8 @@ payload_size = 13 & mask_8bit # 1 for command option, 4 for pos, 4 for speed, 4 
 first_byte = (type_select | payload_size) & mask_8bit
 command_option = 3 & mask_8bit # move
 pos = 100 & mask_32bit
-speed = 1000 & mask_32bit
-accel = 5000 & mask_32bit
+speed = 2000 & mask_32bit
+accel = 10000 & mask_32bit
 pos2 = 0 & mask_32bit
 
 microstep_payload_size = 2
