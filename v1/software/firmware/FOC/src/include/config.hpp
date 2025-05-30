10270978e8d33d309809bd14b9a61e49a642a452
Froggy111 <lijiayi4789@gmail.com>
2025-05-30 09:32:59 +0800

    _write usb implementation, global include dirs

---
+++ v1/software/firmware/FOC/src/include/config.hpp
@@ -0,0 +1,11 @@
+// configs for FOC firmware
+
+/**
+ * @brief Target for stdio functions (printf)
+ * @options USB, CANFD, UART
+ */
+#define STDIO_TARGET STDIO_USB
+#define STDIO_NULL 0
+#define STDIO_USB 1
+#define STDIO_CANFD 2
+#define STDIO_UART 3
--- v1/software/firmware/FOC/src/peripherals/usb/usb.cpp
+++ v1/software/firmware/FOC/src/peripherals/usb/usb.cpp
@@ -2,10 +2,32 @@
 
 #include <stm32g4xx_hal.h>
 
+#include "config.hpp"
 #include "error.hpp"
 
-#if PRINTF_USB == 1
+#if STDIO_TARGET == STDIO_USB
 
-extern "C" {}
+extern "C" {
+
+#ifdef __GNUC__
+
+int _write(int file, char *ptr, int len) {
+  if (file != 1 && file != 2) {
+    return -1;
+  }
+
+  usb::write((uint8_t *)ptr, len);
+  return len;
+}
+
+#else
+
+int fputc(int ch, FILE *f) {
+  usb::write((uint8_t *)&ch, 1);
+  return ch;
+}
+
+#endif
+}
 
 #endif
