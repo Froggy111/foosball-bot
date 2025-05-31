d795f009aa96db1336aab83efeedac960a386505
Froggy111 <lijiayi4789@gmail.com>
2025-05-31 22:26:24 +0800

    config

--- v1/software/firmware/FOC/src/include/config.hpp
+++ v1/software/firmware/FOC/src/include/config.hpp
@@ -9,3 +9,19 @@
 #define STDIO_USB 1
 #define STDIO_CANFD 2
 #define STDIO_UART 3
+
+/**
+ * @brief Debug output settings. Comment out defines to disable a log level.
+ */
+#ifndef NDEBUG
+
+#define DEBUG_ENABLED
+#define DEBUG_LEVEL_TRACE
+#define DEBUG_LEVEL_DEBUG
+#define DEBUG_LEVEL_LOG
+#define DEBUG_LEVEL_NOTE
+#define DEBUG_LEVEL_WARN
+#define DEBUG_LEVEL_ERROR
+#define DEBUG_LEVEL_FATAL
+
+#endif
